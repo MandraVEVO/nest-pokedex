51e65a073909ea460b6c22a4bbd53d9e92dcb4b4
MandraVEVO <paratrabajosquesenecesiten15@gmail.com>
2025-04-22 00:45:07 -0600

    Implementacion de custom provider

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,25 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import axios, { Axios, AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
 import { CreatePokemonDto } from 'src/pokemon/dto/create-pokemon.dto';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
   
-  private readonly axios: AxiosInstance = axios;
+  // private readonly axios: AxiosInstance = axios;
   //provider a continuacion
+
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+    private readonly http:AxiosAdapter
   ){}
   async executeSeed() {
 
     await this.pokemonModel.deleteMany({}); //eliminar todos los registros de la base de datos
 
-    const {data} = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650') //esto crea una dependencia oculta 
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650') //esto crea una dependencia oculta 
     
     const pokemonToInsert: {name: string, no: number}[] = [];
 
